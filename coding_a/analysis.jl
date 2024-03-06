841d6a56f6142c504287f0ccdf627752198dabf9
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-03-06 12:46:23 +0100

    JACKKNIFE CORRECTED FINALLYYYYYYYYYYYYYYY

--- coding_a/analysis.jl
+++ coding_a/analysis.jl
@@ -2,6 +2,7 @@ using CSV, DataFrames, Dates, Plots, Statistics
 
 function JackKnife(e::Array, m::Array, blocksize::Int)
     length = size(e,1) ÷ blocksize 
+
     m_j = Vector{Float64}(undef, length) 
     e_j = Vector{Float64}(undef, length) 
     m_abs_j = Vector{Float64}(undef, length)
@@ -12,37 +13,35 @@ function JackKnife(e::Array, m::Array, blocksize::Int)
     e_cut = e[1:length*blocksize]
     m_cut = m[1:length*blocksize]
 
-    e_b = mean(reshape(e_cut, (blocksize, length)), dims = 1)
-    m_b = mean(reshape(m_cut, (blocksize, length)), dims = 1)
+    e_b = reshape(e_cut, (blocksize, length))
+    m_b = reshape(m_cut, (blocksize, length))
 
-    for i = 0:length-1
-        e_j[i+1] = mean([e_b[1:i]; e_b[(i+1):end]])
-        e2_j[i+1] = mean(abs2, [e_b[1:i]; e_b[(i+1):end]])
-        m_abs_j[i+1] = mean(abs, [m_b[1:i]; m_b[(i+1):end]])
-        m_abs2_j[i+1] = mean(abs2, [m_b[1:i]; m_b[(i+1):end]])
-        m_abs4_j[i+1] = mean((abs.([m_b[1:i]; m_b[(i+1):end]])).^4)
-    end
+    e_j = mean(e_b, dims = 1)
+    e2_j = mean(x->x^2,e_b, dims = 1)
+    m_abs_j = mean(abs, m_b, dims = 1)
+    m_abs2_j = mean(abs2, m_b, dims = 1)
+    m_abs4_j = mean(x->abs(x)^4, m_b, dims = 1)
 
     spec_heat = e2_j.-e_j.^2 
     susc = m_abs2_j.-m_abs_j.^2
     bind = m_abs4_j./(m_abs2_j.^2)
     
-    vars = [e_j m_abs_j m_abs2_j spec_heat susc bind]
-    media = mean(vars, dims = 1)
-    return media, stdm(vars, media, dims = 1)*sqrt(length)
+    vars = [e_j; m_abs_j; m_abs2_j; spec_heat; susc; bind]
+    media = mean(vars, dims = 2)
+    return media, stdm(vars, media, dims = 2).*sqrt(length)
 end
 
-L = 4
-betarray = LinRange(0.4,0.47,10)
+L = 80
+betarray = LinRange(0.85,0.91,21)
 path = "..\\simulations_a\\" 
 f_w =  path*"data"*"L=$L"*".csv"
 touch(f_w)
 
 tosave = DataFrame([[],[],[],[],[],[],[],[],[],[],[],[],[],[]], ["beta", "m", "e","e_v","m_abs","m_abs_v","m_abs2","m_abs2_v","spec_heat","spec_heat_v","susc","susc_v","bind","bind_v"])
 
-for β in betarray
+for (i,β) in enumerate(betarray)
     # start = now()
-    f1 = path*"clock_Nt=1e4"*"L=$L"*"beta=$β.csv"
+    local f1 = path*"clock_Nt=1e5"*"L=$L"*"beta=$β.csv"
     df= CSV.read(f1, DataFrame, types = [Float64, ComplexF64])
     # @show df
     # elapsed = Dates.canonicalize(Dates.round((now()-start), Dates.Second))
@@ -51,16 +50,16 @@ for β in betarray
     energ = df[!,:E]
     magn = df[!,:m]
 
-    # start = now()
+    start = now()
 
-    means, stds = JackKnife(energ,magn,1000)
+    @show means, stds = JackKnife(energ,magn,1000)
 
     inter = ([means stds]'[:])'
     data = [β abs(mean(magn)) inter]
 
-    # elapsed = Dates.canonicalize(Dates.round((now()-start), Dates.Second))
-    # println("Computation took: $(elapsed)")
     push!(tosave, data)
+    println("β = $β,$i/$(size(betarray,1))")
 end
+
 
 CSV.write(f_w, tosave)
