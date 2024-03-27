6215d2a0d09bd2dfeb6f8909618c675d4657bad2
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-03-27 18:29:36 +0100

    first

--- coding_a/plots.jl
+++ coding_a/plots.jl
@@ -1,15 +1,16 @@
 using CSV, DataFrames, Dates, Plots, Statistics
 
 p = plot()
-for size in [80]
-    f = "..\\simulations_a\\dataL=$size.csv"
+for size in [20 40 50 60 70]
+    f = "..\\simulations_a\\data\\dataL=$size.csv"
 
     df= CSV.read(f, DataFrame)
+    println(names(df))
 
     x = df[!,:beta]
-    y = df[!,:spec_heat]
-    sy = df[!,:spec_heat_v]
-    plot!(p,x,y, yerr = sy, )
+    y = df[!,:m_abs2]
+    sy = df[!,:m_abs2_v]
+    plot!(p,x,y, yerr = sy, label = "$size")
 end
 
 p
