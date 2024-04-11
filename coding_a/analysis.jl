e9ef26b31e684bad9fe4c2094fc4fffa5315b20c
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-04-11 18:39:08 +0200

    minor

--- coding_a/analysis.jl
+++ coding_a/analysis.jl
@@ -2,12 +2,14 @@ using CSV, DataFrames, Dates, Plots, Statistics
 include("clock.jl")
 
 size_list = [20, 40, 50, 60, 70, 80]
+size_list = 20
+
 
 for L in size_list
     if L == 60
         betarray = LinRange(0.85,0.91,41)
     else
-        betarray =round.(LinRange(0.85,0.91,41), digits = 4)
+        @show betarray =round.(LinRange(0.85,0.91,41), digits = 4)
     end
     path = "..\\simulations_a\\" 
     f_w =  path*"data"*"L=$L"*".csv"
@@ -23,15 +25,16 @@ for L in size_list
         energ = df[!,:E]
         magn = df[!,:m]
 
+        therm = 10000
         start = now()
 
         
-        means, stds = JackKnife(energ,magn,1500, L)
+        means, stds = JackKnife(energ[therm:end],magn[therm:end],2000, L)
         inter = ([means' stds']'[:])'
         data = [β abs(mean(magn)) inter]
 
         push!(tosave, data)
-        println("β = $β,$i/$(size(betarray,1))")
+        println("$L: β = $β,$i/$(size(betarray,1))")
     end
 
 
