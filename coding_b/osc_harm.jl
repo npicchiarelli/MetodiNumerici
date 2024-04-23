a4bc39c64bdc9c956cf9d513dac0c2f67d33bdb5
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-04-23 15:21:09 +0200

    correlators

--- coding_b/osc_harm.jl
+++ coding_b/osc_harm.jl
@@ -1,3 +1,5 @@
+using LinearAlgebra
+
 function calc_x(lattice::Array{Float64}, Nt::Int)
     return sum(lattice)/Nt    
 end
@@ -20,11 +22,11 @@ function calc_Knaive(lattice::Array{Float64}, Nt::Int, eta::Float64)
     return sum(diffs.*diffs)/(2*Nt*eta*eta)
 end
 
-function metropolis!(lattice::Array{Int}, r::IndexLinear, Δ::Int, β::Float64, η::Float64, nnl::Int, nnr::Int)
+function metropolis!(lattice::Array{Float64}, r::IndexLinear, Δ::Int, β::Float64, η::Float64, nnl::Int, nnr::Int)
     ΔS = Δ*(2*rand(Float64)-1)
     trial = (lattice[r]+ΔS)
-    Eold = lattice[r]*lattice[r]*(η/2.+1./η)-lattice[r]*(lattice[nnl[r]]+lattice[nnr[r]])/η
-    Enew = trial*trial*(η/2.+1./η)-trial*(lattice[nnl[r]]+lattice[nnr[r]])/η
+    Eold = lattice[r]*lattice[r]*(η/2.0+1.0/η)-lattice[r]*(lattice[nnl[r]]+lattice[nnr[r]])/η
+    Enew = trial*trial*(η/2.0+1.0/η)-trial*(lattice[nnl[r]]+lattice[nnr[r]])/η
 
     if Enew < Eold
         lattice[r] = trial
@@ -37,9 +39,27 @@ function metropolis!(lattice::Array{Int}, r::IndexLinear, Δ::Int, β::Float64, 
     return 0
 end
 
-function heathbath!(lattice::Array{Int}, r::IndexLinear, eta::Float64, nnl::Int, nnr::Int)
-    std = 1./sqrt(eta + 2./eta)
-    avg = (lattice[nnl[r]]+lattice[nnr[r]])/(eta*(eta + 2./eta))
+function heathbath!(lattice::Array{Float64}, r::IndexLinear, eta::Float64, nnl::Int, nnr::Int)
+    std = 1.0/sqrt(eta + 2.0/eta)
+    avg = (lattice[nnl[r]]+lattice[nnr[r]])/(eta*(eta + 2.0/eta))
     
-    lattice[r] = 
+    lattice[r] = avg+std*randn()
+
+    return 1
+end
+
+function overrelax(lattice::Array{Float64}, r::IndexLinear, eta::Float64, nnl::Int, nnr::Int)
+    avg = (lattice[nnl[r]]+lattice[nnr[r]])/(eta*(eta + 2.0/eta))
+    new = 2.0*avg-lattice[r]
+    lattice[r] = new
+
+    return 1
+end
+
+function correlators(lattice::Array{Float64}, δt::Int)
+    corr1 = dot(lattice, circshift(lattice, δt))
+    corr2 = dot(lattice.^2,circshift(lattice, δt).^2)
+    corr3 = dot(lattice.^3,circshift(lattice, δt).^3)
+    corr3c = dot(map(x->x^3-1.5x, lattice), map(x->x^3-1.5x, circshift(lattice, δt)))
+    return [corr1 corr2 corr3 corr3c]
 end
