0ee7156439611fdb1fbbab6153ae121bbe310cc6
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-05-14 15:51:01 +0200

    file printing

--- coding_b/analysis_ho_sp_shell.jl
+++ coding_b/analysis_ho_sp_shell.jl
@@ -1,4 +1,4 @@
-using ArgParse, DelimitedFiles, LinearAlgebra, Plots, Printf, Statistics
+using ArgParse, DelimitedFiles, LinearAlgebra, Printf, Statistics, CSV, DataFrames
 include("harm_osc.jl")
 
 function parse_cmd()
@@ -38,49 +38,59 @@ function main()
     therm = parsed_args["therm"]
     sample = parse_args["sample"]
     dfname = parse_args["name"]
-    startp = @sprintf "ho_th_sample=%.1e" sample
+    startp = @sprintf "ho_sp_sample=%.1e" sample
     paths = filter(startswith(startp), readdir(path))
-    size_list = []
-    eta_list = []
-    x = []
-    xv = []
-    x2 = []
-    x2v = []
-    K = []
-    Kv = []
-    H = []
-    Hv = []
 
-    for (i,fname) in enumerate(paths)
-        Nt = parse(Int, fname[end-5:end-4])
-        β = parse(Float64, fname[end-11:end-9])
+    for fname in paths
+        bend = findfirst("beta=", fname)[end]
+        Nstr = findfirst("Nt=", fname)[1]
+        Nend = findfirst("Nt=", fname)[end]
+        Nt = parse(Int, fname[Nend+1:end-4])
+        β = parse(Float64, fname[bend+1:Nstr-1])
         η = β/Nt
-
+    
         local w = open(joinpath([path, fname]), "r") do io
             readdlm(io, header = true)
         end
-
-        x_j = JackKnife(w[1][therm:end,1], blocksize)
-        x2_j = JackKnife(w[1][therm:end,2], blocksize)
-        K_j = JackKnife(w[1][therm:end,3], blocksize)
-        H_j = 0.5.*x2_j.-K_j .+ 1/(2*η)
         
-        push!(size_list, Nt)
-        push!(eta_list, η)
-        push!(x, mean(x_j))
-        push!(x2, mean(x2_j))
-        push!(K, mean(K_j))
-        push!(H, mean(H_j))
+        spacing=Vector{Int}()
+        for el in w[2][2:end]
+            append!(spacing, parse(Int, el[4:end]))
+        end
+    
+        unique!(spacing)
+        cormat = w[1][therm+1:end,:]
+        datajack = zeros(size(cormat,1)÷blocksize, size(cormat,2))
+    
+        for i in 1:size(cormat,2)
+            datajack[:,i] = JackKnife(cormat[:,i], blocksize)
+        end
+    
+        corrected = datajack
+        corrected[:,3:4:end].-=(corrected[:,1].^2)
+        corrected = corrected[:,2:end]
+        gaps = log.(corrected./circshift(corrected, (0,-4)))
+        errs = std(gaps, dims = 1, corrected = false).*sqrt(size(datajack,1)-1)
+        gaps = mean(gaps, dims = 1)
+        gaps = gaps[:,1:end-4]./η
+        errs = errs[:,1:end-4]./η
 
-        push!(xv, std(x_j, corrected = false).*sqrt(length(x_j)-1))
-        push!(x2v, std(x2_j, corrected = false).*sqrt(length(x_j)-1))
-        push!(Kv, std(K_j, corrected = false).*sqrt(length(x_j)-1))
-        push!(Hv, std(H_j, corrected = false).*sqrt(length(x_j)-1))
-    end
-
-    w = open(joinpath([path, dfname]), "w") do io
-        writedlm(io, ["size_list" "eta_list" "x" "xv" "x2" "x2v" "K" "Kv" "H" "Hv"], " ")
-        writedlm(io, [size_list eta_list x xv x2 x2v K Kv H Hv], " ")
+        data = DataFrame(
+            sp = spacing[1:end-1],
+            x = gaps[1:4:end],
+            xv = errs[1:4:end],
+            x2 = gaps[2:4:end],
+            x2v = errs[2:4:end],
+            x3 = gaps[3:4:end],
+            x3v = errs[3:4:end],
+            x4 = gaps[4:4:end],
+            x4v = errs[4:4:end],
+        )
+        if !isdir(joinpath([path, "data"]))
+            mkpath(joinpath([path, "data"]))
+        end
+        f1 = @sprintf "data_sample%.1eNt%ibeta%.1f.csv" sample Nt β
+        touch(joinpath([path, "data", f1]))
+        CSV.write(joinpath([path, "data", f1]), data) 
     end
 end
-
