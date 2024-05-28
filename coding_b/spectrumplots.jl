7df8ba863f2d19605fe6fda6658dabdbc2a17d6d
npicchiarelli <picchiarelliniccolo@gmail.com>
2024-05-28 17:14:05 +0200

    Added Annotation

--- coding_b/spectrumplots.jl
+++ coding_b/spectrumplots.jl
@@ -1,5 +1,12 @@
-using DelimitedFiles, Plots, Printf, Statistics
+using CSV, DataFrames, DelimitedFiles, Plots, Printf, Statistics
 include("harm_osc.jl")
+
+default(fontfamily = "Computer Modern",
+background_color = :white,
+foreground_color = :black,
+# background_color_legend = nothing,
+margin=5Plots.mm
+)
 
 path = "..\\simulations_b\\"
 sample, Nt, β = 100000000, 60, 10.
@@ -7,11 +14,15 @@ fname = @sprintf "data_sample%.1eNt%ibeta%.1f.csv" sample Nt β
 
 df= CSV.read(joinpath([path, fname]), DataFrame)
 
-p = plot()
+p = plot(legend=:best)
 
-scatter!(p, df[!,:sp],df[!,:x], yerr = df[!,:xv], markershape=:plus)
-scatter!(p, df[!,:sp],df[!,:x2], yerr = df[!,:x2v], markershape=:plus)
-scatter!(p, df[!,:sp],df[!,:x3], yerr = df[!,:x3v], markershape=:plus)
-scatter!(p, df[!,:sp],df[!,:x4], yerr = df[!,:x4v], markershape=:plus)
+scatter!(p, df[!,:sp],df[!,:x], yerr = df[!,:xv], markershape=:plus, label = L"x-x")
+scatter!(p, df[!,:sp],df[!,:x2], yerr = df[!,:x2v], markershape=:plus, label = L"x^2-x^2")
+scatter!(p, df[!,:sp],df[!,:x3], yerr = df[!,:x3v], markershape=:plus, label = L"x^3-x^3")
+scatter!(p, df[!,:sp],df[!,:x4], yerr = df[!,:x4v], markershape=:plus, label = L"A-A")
+xlabel!("Spacing")
+ylabel!("Correlators")
+ylims!(0.9, 4)
+annotate!(p, 2.8, 3.35, text(L"Where $A = x^3-\frac{3}{2}x$", :black,:left, 8))
 
 display(p)
